9965b1aa223a61e61bef0aa3139d562538c02d00
Ymaro Blue <coding.ymaro@gmail.com>
2024-02-25 02:08:35 +0100

    string updates

--- src/composables/paddingSizes.ts
+++ src/composables/paddingSizes.ts
@@ -1,11 +1,14 @@
-import { twMerge } from 'tailwind-merge'
-import { computed } from 'vue'
+import { twMerge } from "tailwind-merge";
+import { computed } from "vue";
 
 export function usePaddingSizes(size: string) {
-    return computed(() => twMerge([
-        size === 'sm' && 'px-2 py-1 text-sm rounded-sm',
-        size === 'md' && 'px-4 py-2 text-base rounded-md',
-        size === 'lg' && 'px-6 py-3 text-lg rounded-lg',
-        size === 'xl' && 'px-8 py-4 text-xl rounded-xl',
-    ]))
+    return computed(() =>
+        twMerge([
+            size === "xs" && "px-1 py-1 text-xs rounded-sm",
+            size === "sm" && "px-2 py-1 text-sm rounded",
+            size === "md" && "px-4 py-2 text-base rounded-md",
+            size === "lg" && "px-6 py-3 text-lg rounded-lg",
+            size === "xl" && "px-8 py-4 text-xl rounded-xl",
+        ]),
+    );
 }
