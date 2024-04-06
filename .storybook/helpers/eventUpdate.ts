dfd691a263a5d1e7b9ea772e5a3ec1a2fc5037cd
Ymaro Blue <coding.ymaro@gmail.com>
2024-04-06 16:31:36 +0200

    storybook update

--- .storybook/helpers/eventUpdate.ts
+++ .storybook/helpers/eventUpdate.ts
@@ -1,32 +1,30 @@
-import type { Channel } from '@storybook/channels'
-import { UPDATE_STORY_ARGS } from '@storybook/core-events'
-import type { Decorator } from '@storybook/vue3'
-import { h } from 'vue'
+import type { Channel } from "@storybook/channels";
+import { UPDATE_STORY_ARGS } from "@storybook/core-events";
+import type { Decorator } from "@storybook/vue3";
+import { h } from "vue";
 
 export function updateModelValue(): Decorator {
-    return (story, ctx) => {
-        return {
-            components: { story },
-            setup() {
-                const updateArgs = (value: unknown) => {
-                    const channel = (window as any).__STORYBOOK_ADDONS_CHANNEL__ as Channel
-                    channel.emit(UPDATE_STORY_ARGS, {
-                        storyId: ctx.id,
-                        updatedArgs: {
-                            modelValue: value,
-                        },
-                    })
-                }
+    return (story, ctx) => ({
+        components: { story },
+        setup() {
+            const updateArgs = (value: unknown) => {
+                const channel = (window as any).__STORYBOOK_ADDONS_CHANNEL__ as Channel;
+                channel.emit(UPDATE_STORY_ARGS, {
+                    storyId: ctx.id,
+                    updatedArgs: {
+                        modelValue: value,
+                    },
+                });
+            };
 
-                return {
-                    updateArgs,
-                }
-            },
-            render() {
-                return h(story(), {
-                    'onUpdate:modelValue': (e: any) => this.updateArgs(e),
-                })
-            },
-        }
-    }
+            return {
+                updateArgs,
+            };
+        },
+        render() {
+            return h(story(), {
+                "onUpdate:modelValue": (e: any) => this.updateArgs(e),
+            });
+        },
+    });
 }
--- .storybook/viteStorybook.config.ts
+++ .storybook/viteStorybook.config.ts
@@ -1,40 +1,41 @@
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@/components': resolve(__dirname, '../src/components'),
-            '@/stores': resolve(__dirname, '../src/stores'),
-            '@/composables': resolve(__dirname, '../src/composables'),
-            '@/utils': resolve(__dirname, '../src/utils'),
-            '@public': resolve(__dirname, '../public'),
+            "@/components": resolve(__dirname, "../src/components"),
+            "@/modules": resolve(__dirname, "../src/modules"),
+            "@/stores": resolve(__dirname, "../src/stores"),
+            "@/composables": resolve(__dirname, "../src/composables"),
+            "@/utils": resolve(__dirname, "../src/utils"),
+            "@public": resolve(__dirname, "../public"),
         },
         extensions: [
-            '.mjs',
-            '.js',
-            '.mts',
-            '.ts',
-            '.jsx',
-            '.tsx',
-            '.json',
-            '.vue',
+            ".mjs",
+            ".js",
+            ".mts",
+            ".ts",
+            ".jsx",
+            ".tsx",
+            ".json",
+            ".vue",
         ],
     },
     define: {
-        'process.env': {},
-        ...(process.env.NODE_ENV === 'production'
+        "process.env": {},
+        ...(process.env.NODE_ENV === "production"
             ? {
-                  global: 'global',
-              }
+                global: "global",
+            }
             : {}),
     },
     build: {
         rollupOptions: {
-            external: ['@storybook/testing-library'],
+            external: ["@storybook/testing-library"],
         },
     },
 });
