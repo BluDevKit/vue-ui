ba273bb8a2166cb2dbb05433fd091a38b565f98f
Ymaro Blue <coding.ymaro@gmail.com>
2024-04-06 15:22:41 +0200

    storybook update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,40 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from "url";
+import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
     framework: {
-        name: '@storybook/vue3-vite',
+        name: "@storybook/vue3-vite",
         options: {
             builder: {
-                viteConfigPath: fileURLToPath(
-                    new URL('./viteStorybook.config.ts', import.meta.url)
-                ),
+                viteConfigPath: fileURLToPath(new URL("./viteStorybook.config.ts", import.meta.url)),
             },
         },
     },
     docs: {
-        autodocs: 'tag',
+        autodocs: "tag",
         docsMode: false,
     },
     stories: [
         {
-            titlePrefix: 'Vue UI/',
-            directory: '../src/components',
-            files: '**/*.stories.@(js|jsx|ts|tsx)',
+            titlePrefix: "Vue UI Components/",
+            directory: "../src/components",
+            files: "**/*.stories.@(js|jsx|ts|tsx)",
         },
         {
-            titlePrefix: 'Guide/',
-            directory: './guide',
-            files: '**/*.mdx',
+            titlePrefix: "Vue UI Modules/",
+            directory: "../src/modules",
+            files: "**/*.stories.@(js|jsx|ts|tsx)",
+        },
+        {
+            titlePrefix: "Guide/",
+            directory: "./guide",
+            files: "**/*.mdx",
         },
     ],
     addons: [
-        '@storybook/addon-essentials',
-        // 'storybook-addon-pseudo-states',
-        '@storybook/addon-interactions',
-        '@storybook/addon-themes',
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-themes",
     ],
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,12 @@
-import { bluDevKitUtils } from './../src/utils/toast';
-import { type Preview, setup } from '@storybook/vue3';
-import { extractArgTypes, updateModelValue } from './helpers';
-import '@storybook/test';
+import { type Preview, setup } from "@storybook/vue3";
+import { createPinia } from "pinia";
+import { App } from "vue";
+import { bluDevKitUtils } from "../src/utils/toast";
+import { extractArgTypes, updateModelValue } from "./helpers";
+import "@storybook/test";
 
-import '../src/assets/style.css';
-import './assets/stories.scss';
-import { App } from 'vue';
-import { createPinia } from 'pinia';
+import "../src/assets/style.css";
+import "./assets/stories.scss";
 
 const pinia = createPinia();
 
@@ -19,14 +19,15 @@ setup((app: App) => {
 
 const preview: Preview = {
     parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
         options: {
             storySort: {
-                method: 'configure',
+                method: "configure",
                 includeNames: true,
                 order: [
-                    'Guide',
-                    ['What is Blu Dev Kit', 'Getting Started'],
-                    '*',
+                    "Guide",
+                    ["What is Blu Dev Kit", "Getting Started"],
+                    "*",
                 ],
             },
         },
